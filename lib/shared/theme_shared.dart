ace48b96e11bc1e8439a3250a77d21454b1813a1
adibakbar14 <e32220671@student.polije.ac.id>
2023-09-16 12:47:25 +0700

    update asset-shared

---
+++ lib/shared/theme_shared.dart
@@ -0,0 +1,8 @@
+part of 'shared.dart';
+
+Color primaryColor = Color(0xFFFFFF); //warnaputih
+Color secondaryColor = Color(0x2F6D53); //warnaijotua
+Color dangerColor = Color(0xE50000); //warnamerah
+Color blackColor = Color(0x1E1E1E); //warnahitam
+ 
+TextStyle Jenisfont = GoogleFonts.
